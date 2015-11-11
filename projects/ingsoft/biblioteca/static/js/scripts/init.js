83fc890097b4fd72a76c4af1b837dc5cf9228f94
stevensotelo <stevenbetancurt@hotmail.com>
2015-11-11 00:51:08 -0500

    se ajusta el proyecto 2

--- projects/ingsoft/biblioteca/static/js/scripts/init.js
+++
@@ -1,9 +0,0 @@
-$(this).ready(function(){
-    MAPS.tools.init('map-canvas');
-    if(MAPS.vars.grandient==[])
-        MAPS.tools.generateGradient(100);
-    $(".content-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-});
