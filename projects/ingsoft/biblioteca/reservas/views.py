2f1dfa552c5358e2f1c3e90c2cc354ee70b0e4c5
stevensotelo <stevenbetancurt@hotmail.com>
2015-11-11 22:23:28 -0500

    se ajustan la aplicacion reserva

--- projects/ingsoft/biblioteca/reservas/views.py
+++ projects/ingsoft/biblioteca/reservas/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 
+# Create your views here.
 def reserva_consulta(request):
-    
     return render_to_response('reserva_consulta.html',{},RequestContext(request))
     
-    
+@csrf_exempt    
 def reserva_registro(request):
-    
-    return render_to_response('reserva_registro.html',{},RequestContext(request))
+    v={}
+    if request.method == 'POST':        
+        accion = request.POST.get("hdnAccion", "")
+        print(accion)
+        if accion == "1":
+            v={'view_documento':False,'view_libro':False,'libro':'Mi Libro','autor':'Steven','descripcion':'Un libro','material':'Papel','clasificacion':'Accion','isbn':'kdjfsldfjsd'}
+            print(v)
+            return render_to_response('reserva_registro.html',v,RequestContext(request))
+        else:
+            v={'view_documento':False,'view_libro':False}
+            return render_to_response('reserva_registro.html',v,RequestContext(request))
+    else:
+        v={'view_documento':False,'view_libro':False}
+        return render_to_response('reserva_registro.html',v,RequestContext(request))
